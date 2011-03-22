d2066b7c320f15421ecbe9ffd2a10056fe0e16b5
Adriana Pahoncea <adriana.pahoncea@yahoo.com>
2011-03-22 10:37:33 +0200

    Search widget behavior (JQuery implem.)

---
+++ javascripts/jquery.searchBehavior.js
@@ -0,0 +1,42 @@
+jQuery.fn.searchBehavior = function() {
+    var search = this.find('.search_field');
+    var clear = this.find('.clear_search');    
+    var submit = this.find('.submit_search');	
+
+    function updateClearSearch() {
+        if (search.val().length > 0) {
+            clear.show();
+        } else {
+            clear.hide();
+        }
+    }
+	
+    updateClearSearch();
+
+    search.keyup(function(){
+        updateClearSearch();
+        search.focus();
+    });
+
+    search.keypress(function(e){
+        if(e.keyCode==13) {        
+            clear.hide();			
+			alert("You have used the search widget!");
+			updateClearSearch();
+        }
+    });
+
+    clear.click(function(){
+        search.val("");
+        search.focus();
+        updateClearSearch();
+    });
+    
+    submit.click(function(){
+        clear.hide();   
+		alert("You have used the search widget!");
+		updateClearSearch();
+    });
+};
+
+  
